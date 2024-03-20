f1985d93a8cf9e20fbdb8b2b9b5e4e603e19ff1c
EvanPWilson <145310929+EvanPWilson@users.noreply.github.com>
2024-03-19 21:01:42 -0700

    Update UHSC.sh

--- UHSC.sh
+++ UHSC.sh
@@ -6,11 +6,14 @@ target_directory="/home/sysadmin/Downloads/UserHistoryCollection"
 # Create the directory if it doesn't exist
 mkdir -p "$target_directory"
 
-# Path to the user's Bash history file
-history_file_path="${HOME}/.bash_history"
+username=$1
+
+# Define the path to the user's command history file
+history_file_path="/home/$username/.bash_history"
 
 # Target file where you want to append the history
-target_file="${target_directory}/userhistory.txt"
+current_time=$(date "+%Y-%m-%d_%H-%M-%S")
+target_file="${target_directory}/userhistory_${username}_${current_time}.txt"
 
 # Check if the history file exists then append its content to the target file
 if [ -f "$history_file_path" ]; then
